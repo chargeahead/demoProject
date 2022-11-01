91050f5b3c1919e2ed1d15863e5c47ca7157663f
kamalnathkn <64536036+kamalnathkn@users.noreply.github.com>
2022-11-01 15:57:17 +0530

    Update PersonController.java

--- src/main/java/controller/PersonController.java
+++ src/main/java/controller/PersonController.java
@@ -23,6 +23,7 @@ public class PersonController {
 	@RequestMapping("/all")
 	public ArrayList<Person> getAll() {
 		System.out.println("newly created branch  1");
+		System.out.println("newly created branch  1");
 		return ps.getAll();
 	}
 
